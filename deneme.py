1a97ae6b26d27e9147373ce0479d44d8a67883ad
Asikarcelik <asikarcelik4@gmail.com>
2025-04-28 15:11:26 +0300

    Gereksiz dosya silindi

--- deneme.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-# Şifrenizi girin
-raw_password = "adminasikar"  # Yeni adminin şifresi
-
-# Hashli şifre oluşturma
-hashed_password = generate_password_hash(raw_password)
-
-print("Hashli Şifre:", hashed_password)
